9d7a3760581e2c18fd73bbff0531200e32353fd1
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2019-05-09 10:14:43 -0300

    .

--- lib/App.class.php
+++ lib/App.class.php
@@ -2,70 +2,88 @@
 namespace lib;
 
 use Exception;
+use zion\utils\HTTPUtils;
 
+/**
+ * @author Vinicius Cesar Dias
+ * @since 08/05/2019
+ */
 class App {
     public static function route(){
         // parâmetros
         $uri        = $_SERVER["REQUEST_URI"];
         $servername = $_SERVER["SERVER_NAME"];
         
-        $cacheKey   = str_replace("/","_",strtolower($uri));
+        $cacheKey   = str_replace(array("/","-"),"_",strtolower($uri));
         $cacheKey   = preg_replace("[^a-z0-9]","",$cacheKey);
         
-        $cacheFolder = zion\APP_ROOT."tmp".\DS."cache".\DS.$servername.\DS;
+        $cacheFolder = \zion\APP_ROOT."tmp".\DS."cache".\DS.$servername.\DS;
         $cacheFile  = $cacheFolder.$cacheKey;
         
-        @mkdir($cacheFolder,0777,true);
-        chmod($cacheFolder,0777);
+        if(!file_exists($cacheFolder)){
+            @mkdir($cacheFolder,0777,true);
+            chmod($cacheFolder,0777);
+        }
         
         try {
             $config = CDN::getDomainConfig($servername);
-            
             if(CDN::byPass()){
                 $xCache = "BYPASS";
                 
-                header("Cache-Control: must-revalidate");
-                $offset = strtotime('-10 minute');
-                header("Expires: ".gmdate("D, d M Y H:i:s", $offset)." GMT");
+                header("Cache-Control: must-revalidate, max-age: 0, s-maxage: 0, private");
+                header("Pragma: no-cache");
                 header("x-cache: ".$xCache." on ".$_SERVER["SERVER_ADDR"]);
                 
-                echo CDN::getOriginalContent($config);
+                $response = CDN::getOriginalContent($config);
+                
+                $fileInfo = array(
+                    "headers" => $response["lastHeaders"],
+                    "content" => $response["body"]
+                );
+                
+                CDN::send($fileInfo,$xCache);
             }else{
                 $xCache = "MISS";
                 
                 // se o cache existe e ainda é válido, usa o cache
+                $fileInfo = null;
                 if(file_exists($cacheFile)){
-                    $maxTime = CDN::getCacheTime();
+                    $fileInfo = CDN::loadCache($cacheFile);
                     $lifetime = CDN::getLifetimeFile($cacheFile);
-                    
-                    if($lifetime < $maxTime){
+                    if($fileInfo != null AND $lifetime < $fileInfo["smaxage"]){
                         $xCache = "HIT";
                     }
                 }
                 
                 // fazendo a requisição
                 if($xCache == "MISS"){
-                    $responseBody = CDN::getOriginalContent($config);
+                    $response = CDN::getOriginalContent($config);
+                    
+                    $cacheControl = null;
+                    foreach($response["lastHeaders"] AS $header){
+                        if(strtolower($header["key"]) == "cache-control"){
+                            $cacheControl = HTTPUtils::parseCacheControl($header["value"]);
+                        }
+                    }
                     
                     // gravando no cache
-                    $f = fopen($cacheFile,"a+");
-                    fwrite($f,$responseBody);
-                    fclose($f);
+                    CDN::saveCache($cacheFile, $response["lastHeaders"], $response["body"], 
+                        $cacheControl["max-age"], $cacheControl["s-maxage"]);
+                    
+                    // gerando fileinfo
+                    $fileInfo = array(
+                        "headers" => $response["lastHeaders"],
+                        "content" => $response["body"]
+                    );
                 }
                 
-                // cabeçalhos para fazer cache de 1 minuto
-                header("Cache-Control: must-revalidate");
-                $offset = strtotime('+1 minute');
-                header("Expires: ".gmdate("D, d M Y H:i:s", $offset)." GMT");
-                header("x-cache: ".$xCache." on ".$_SERVER["SERVER_ADDR"]);
-                
-                readfile($cacheFile);
-                echo "<!-- servercache ".date("d/m/Y H:i:s")." -->";
+                CDN::send($fileInfo,$xCache);
             }
         }catch(Exception $e){
             header("HTTP/1.1 500");
             echo $e->getMessage();
         }
+        exit();
     }
 }
 ?>
--- lib/CDN.class.php
+++ lib/CDN.class.php
@@ -2,27 +2,99 @@
 namespace lib;
 
 use Exception;
+use zion\utils\HTTPUtils;
 
+/**
+ * @author Vinicius Cesar Dias
+ * @since 08/05/2019
+ */
 class CDN {
     public static function getDomainConfig($servername){
-        return array(
-            "origem" => "0.0.0.0"
-        );
+        $file = \zion\APP_ROOT."cdn.json";
+        
+        if(!file_exists($file)){
+            throw new Exception("O arquivo de configuração não existe");
+        }
+        $content = file_get_contents($file);
+        if($content == ""){
+            throw new Exception("O arquivo de configuração não tem conteúdo");
+        }
+        $json = json_decode($content);
+        if($json == null){
+            throw new Exception("O arquivo de configuração não é um json válido");
+        }
+        
+        foreach($json AS $obj){
+            if($obj->domain == $servername){
+                return $obj;
+            }
+        }
+        
+        throw new Exception("Domínio ".$servername." não cadastrado");
     }
     
-    public static function getOriginalContent($config) : string {
-        $url = "http://".$config["origem"].$_SERVER["REQUEST_URI"];
+    public static function send(array $fileInfo,$xCache=""){
+        $i=0;
+        foreach($fileInfo["headers"] AS $header){
+            $line = "";
+            if($i == 0){
+                $line = $header["key"];
+            }else{
+                $line = $header["key"].": ".$header["value"];
+            }
+            
+            // ignorando esse cabeçalho por enquanto para não causar problemas
+            if($header["key"] == "Transfer-Encoding"){
+                continue;
+            }
+            
+            header($line);
+            $i++;
+        }
+        
+        if($xCache == ""){
+            header("x-zcache: zcdn");
+        }else{
+            header("x-zcache: zcdn (".$xCache.")");
+        }
+        
+        echo $fileInfo["content"];
+    }
+    
+    public static function getOriginalContent($config,&$curlInfo=array()) {
+        $url = "http://".$config->source.$_SERVER["REQUEST_URI"];
         $method = "GET";
         $data = null;
         $options = array(CURLOPT_HTTPHEADER => array("Host: ".$_SERVER["SERVER_NAME"]));
-        $curlInfo = null;
         
-        $responseBody = self::curl($url,$method, $data, $options, $curlInfo);
+        $response = HTTPUtils::curl2($url, $method, $data, $options, $curlInfo);
+        
+        /*
         if($curlInfo["http_code"] != "200"){
             throw new Exception("Erro");
         }
+        */
         
-        return $responseBody;
+        return $response;
+    }
+    
+    public static function saveCache($file,$headers,$content,$maxage,$smaxage){
+        $f = fopen($file,"a+");
+        $fileContent = array(
+            "maxage"  => intval($maxage),
+            "smaxage" => intval($smaxage),
+            "headers" => $headers,
+            "content" => $content
+        );
+        fwrite($f,serialize($fileContent));
+        $fileContent = null;
+        fclose($f);
+    }
+    
+    public static function loadCache($file){
+        $content = file_get_contents($file);
+        $info = unserialize($content);
+        return $info;
     }
     
     /**
@@ -40,145 +112,10 @@ class CDN {
      * @return bool
      */
     public static function byPass() : bool {
-        if($_SERVER["REQUEST_METHOD"] != "GET"){
-            return true;
+        if(in_array($_SERVER["REQUEST_METHOD"],array("GET","HEAD"))){
+            return false;
         }
-        
-        $prefixArray = [];
-        $prefixArray[] = "/modules/";
-        $prefixArray[] = "/servico/";
-        
-        foreach($prefixArray AS $prefix){
-            if (strpos($_SERVER["REQUEST_URI"],$prefix) === 0) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    /**
-     * Tempo em segundos que o cache será válido
-     * @return int
-     */
-    public static function getCacheTime() : int {
-        if (preg_match('/(\.jpg|\.png|\.bmp)$/i', $_SERVER["REQUEST_URI"])) {
-            return 31557600; // 365.25 dias
-        }
-        
-        return 3600; // 1 hora
-    }
-    
-    public static function curl($url, $method = "GET", $data = null, $options = null, &$curlInfo = null) {
-        if (!function_exists("curl_init")) {
-            throw new Exception("A biblioteca curl não esta disponível", -1);
-        }
-        
-        if ($data === null) {
-            $data = array();
-        }
-        
-        if (!is_array($options)) {
-            $options = array();
-        }
-        
-        // opções default
-        if (empty($options)) {
-            $options[CURLOPT_TIMEOUT] = 60;
-            $options[CURLOPT_CONNECTTIMEOUT] = 30;
-            $options[CURLOPT_USERAGENT] = "php";
-        }
-        
-        $ch = curl_init();
-        if ($ch === false) {
-            throw new Exception("Não foi possível initializar curl (curl_init), verifique se a URL " . $url . " esta acessível", -2);
-        }
-        
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 30);
-        
-        // ignora erros de ssl
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        
-        // setando opções definidas pelo usuário
-        foreach ($options AS $key => $value) {
-            curl_setopt($ch, $key, $value);
-        }
-        
-        // método da requisição
-        switch ($method) {
-            case "POST":
-                curl_setopt($ch, CURLOPT_POST, 1);
-                break;
-            case "GET":
-                curl_setopt($ch, CURLOPT_POSTFIELDS, null);
-                curl_setopt($ch, CURLOPT_POST, false);
-                curl_setopt($ch, CURLOPT_HTTPGET, true);
-                break;
-            case "HEAD":
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "HEAD");
-                curl_setopt($ch, CURLOPT_NOBODY, true);
-                break;
-            default:
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-                break;
-        }
-        
-        // dados do corpo da requisição
-        // a função http_build_query já codifica os campos
-        // Atenção! Essa função tem que ser testada com binarios e upload de arquivos
-        if ($data !== null) {
-            if (is_array($data)) {
-                $fieldsString = http_build_query($data);
-                if (!empty($data)) {
-                    // necessário para que o outro lado entenda que os parâmetros estão codificados
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                        'Content-Type: application/x-www-form-urlencoded',
-                        'Cache-Control: no-cache'
-                    ));
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsString);
-                }
-            } elseif (is_string($data) AND trim($data) != "") {
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            }
-        }
-        
-        $response = curl_exec($ch);
-        
-        if ($response === false) {
-            $errorCode = intval(curl_errno($ch));
-            $errorList = array(
-                1 => "Protocolo desconhecido",
-                3 => "URL incorreta",
-                5 => "Host do proxy não encontrado",
-                6 => "Host não encontrado",
-                7 => "Erro em conectar no host ou proxy",
-                9 => "Acesso negado",
-                22 => "Erro na requisição",
-                26 => "Erro na leitura",
-                27 => "Falta de memória",
-                28 => "Timeout",
-                47 => "Limite de redirecionamento atingido",
-                55 => "Erro de rede no envio de dados",
-                56 => "Erro de rede na leitura de dados",
-            );
-            $errorMessage = $errorList[$errorCode];
-            if (mb_strlen($errorMessage) <= 0) {
-                $errorMessage = "Erro desconhecido em executar curl, verifique se a URL " . $url . " esta acessível";
-            }
-            
-            // concatenando informações adicionais
-            $errorMessage = "[" . $errorCode . "][" . $url . "] " . $errorMessage;
-            
-            throw new Exception($errorMessage, $errorCode);
-        }
-        $curlInfo = curl_getinfo($ch);
-        
-        curl_close($ch);
-        return $response;
-    }
+        return true;
+    }  
 }
 ?>
